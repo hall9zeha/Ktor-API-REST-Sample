61d860bf1c07aca4dac95b91dd0d0a23075f956c
hall9zeha <vmwaretars@gmail.com>
2025-07-23 12:06:57 -0500

    Created project and Updated

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,14 @@
+package dev.barryzeha
+
+import dev.barryzeha.plugins.configureRouting
+import dev.barryzeha.plugins.configureSerialization
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
