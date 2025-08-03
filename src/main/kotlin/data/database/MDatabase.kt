eab7c4bb4ea2e3d2219361e5396caad1c569d283
hall9zeha <vmwaretars@gmail.com>
2025-08-03 11:17:29 -0500

    Created user data class

--- src/main/kotlin/data/database/MDatabase.kt
+++ src/main/kotlin/data/database/MDatabase.kt
@@ -1,23 +1,21 @@
 package dev.barryzeha.data.database
 
 import com.mongodb.client.model.Filters
-import com.mongodb.client.model.Updates
 import com.mongodb.kotlin.client.coroutine.MongoClient
 import com.mongodb.kotlin.client.coroutine.MongoCollection
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
 import com.typesafe.config.ConfigFactory
+import dev.barryzeha.model.User
 import dev.barryzeha.model.VideoGame
-import io.ktor.http.ContentType
 import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.flow.toCollection
 import kotlinx.coroutines.flow.toList
-import org.bson.Document
 import org.bson.types.ObjectId
 
 class MDatabase {
     private var mongoClient: MongoClient?=null
     private var database: MongoDatabase?=null
-    private var collection: MongoCollection<VideoGame>?=null
+    private var gameCollection: MongoCollection<VideoGame>?=null
+    private var userCollection: MongoCollection<User>?=null
     companion object {
         private var instance: MDatabase? = null
         fun getInstance(): MDatabase {
@@ -35,34 +33,44 @@ class MDatabase {
         val mongoUri = config.getString("mongo.uri")
         mongoClient = MongoClient.create(mongoUri)
         database = mongoClient?.getDatabase("games_db")
-        collection = database?.getCollection<VideoGame>("game")
+        gameCollection = database?.getCollection<VideoGame>("game")
+        userCollection = database?.getCollection<User>("user")
     }
     suspend fun saveGame(game: VideoGame):VideoGame?{
-        val result =  collection?.insertOne(game)
+        val result =  gameCollection?.insertOne(game)
         val documentId=result?.insertedId
-        return collection?.find(Filters.eq("_id",documentId))?.firstOrNull()
+        return gameCollection?.find(Filters.eq("_id",documentId))?.firstOrNull()
     }
     suspend fun getAllGames():List<VideoGame>{
-        return collection?.find()?.toList()?:emptyList()
+        return gameCollection?.find()?.toList()?:emptyList()
     }
     suspend fun getGamesWithPagination(page:Int, perPage:Int):List<VideoGame>?{
         val skipAmount = (page - 1) * perPage
-        return collection?.find()?.skip(skipAmount)?.limit(perPage)?.toList()?:emptyList()
+        return gameCollection?.find()?.skip(skipAmount)?.limit(perPage)?.toList()?:emptyList()
     }
     suspend fun findById(id: String): VideoGame?{
         val objectId = ObjectId(id)
-        return collection?.find(Filters.eq("_id",objectId))?.firstOrNull()
+        return gameCollection?.find(Filters.eq("_id",objectId))?.firstOrNull()
     }
     suspend fun findByDeveloper(developer:String): List<VideoGame>?{
         val filter = Filters.eq("developer", developer)
-        return collection?.find(filter)?.toList()
+        return gameCollection?.find(filter)?.toList()
     }
     suspend fun updateGame(game: VideoGame){
         val filter = Filters.eq("_id",game.id)
-        collection?.replaceOne(filter,game)
+        gameCollection?.replaceOne(filter,game)
     }
     suspend fun deleteGame(game: VideoGame){
         val filter = Filters.eq("_id", game.id)
-        collection?.deleteOne(filter)
+        gameCollection?.deleteOne(filter)
+    }
+    // User
+    suspend fun saveUser(user: User):User?{
+        val result = userCollection?.insertOne(user)
+        val documentId = result?.insertedId
+        return userCollection?.find(Filters.eq("_id",documentId))?.firstOrNull()
+    }
+    suspend fun getAllUsers():List<User>?{
+        return userCollection?.find()?.toList()?:emptyList()
     }
 }
